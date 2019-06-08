68a593b75ae5b05f0d566f1b1480c2721161e877
David Foote <david.a.foote@gmail.com>
2019-06-08 22:57:09 +0000

    Automatically backed up by Learn

--- tweet_shortener.rb
+++ tweet_shortener.rb
@@ -43,6 +43,7 @@ end
      
 def shortened_tweet_truncator(tweet)
   tweet.split(" ").collect do |array_tweet|
+    binding.pry
     if array_tweet.length > 140
       array_tweet[0..140] + "..."
     else
